530362a897993adfe9095d5055ab3789da1236c0
sra1kumar-NULL <sravansandeep163@gmail.com>
2021-06-14 17:48:08 +0530

    created the snake game

---
+++ snake/src/snake/GameFrame.java
@@ -0,0 +1,17 @@
+package snake;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+	GameFrame(){
+		this.add(new GamePanel());
+		this.setTitle("Snake");
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		
+	}
+
+}
